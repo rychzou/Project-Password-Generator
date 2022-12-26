7e97484740b4d03866361e3bd4e111b2d1733275
Richard Zou <114933397+rychzou@users.noreply.github.com>
2022-12-26 13:35:00 -0500

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ function generatePassword() {
     var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    var specialSet = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "?", "/", "-", ":", ";", "[", "]", "{", "}", ".", "<", ">", "=", "_", "`", "|", "~"];
+    var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "?", "/", "-", ":", ";", "[", "]", "{", "}", ".", "<", ">", "=", "_", "`", "|", "~"];
     var selectedArray = [];
 
     var passwordLength = getPasswordLength();
@@ -33,7 +33,7 @@ function generatePassword() {
         selectedArray = selectedArray.concat(numbers);
     }
     if (specialCharacters) {
-        selectedArray = selectedArray.concat(specialSet);
+        selectedArray = selectedArray.concat(specialChar);
     }
 
     var passwordString = "";
